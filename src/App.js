40f8624261e4179a2d793aed5e4d11fccb2b9f78
yasminemediamacs <yasmine@mediamacs.com>
2021-10-17 21:22:05 +0200

    Added the logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import logo from "./logo.png";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
     </div>
   );
 }
