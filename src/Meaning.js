b66a79e578eab08fcb1a14ea39db138b7c5915c6
yasminemediamacs <yasmine@mediamacs.com>
2021-10-31 11:54:03 +0100

    Cleaned my code

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,18 +3,15 @@ import Synonyms from "./Synonyms";
 import "./Meaning.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <div className="definition">{definition.definition}</div>
-              <div className="example">{definition.example}</div>
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            <div className="definition">{definition.definition}</div>
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ import ReactAudioPlayer from "react-audio-player";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <div className="phonetics-text">/{props.phonetic.text}/</div>
+      <div className="phonetics-text">{props.phonetic.text}</div>
       <br />
 
       <ReactAudioPlayer src={props.phonetic.audio} controls />
